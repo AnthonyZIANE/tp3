ff318067454ecfc68b9734e0dea2e52ce43a0b25
AnthonyZIANE <-Marseille1313->
2020-03-01 17:29:23 +0100

    avancement

--- connexion.php
+++ connexion.php
@@ -3,7 +3,7 @@
 $hote = 'mysql-anthonyziane.alwaysdata.net';
 $base = 'anthonyziane_database';
 $user = '189623';
-$pass = 'voiture12345';
+$pass = 'voiture1313';
 $co = mysqli_connect($hote, $user, $pass) or die(mysqli_connect_error());
 $ret = mysqli_select_db($co, $base) or die (mysqli_connect_error());
 
