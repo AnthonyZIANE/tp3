b73a78db3498693f0621b562646fe7462651da47
z18011806 <anthony.ziane@etu.univ-amu.fr>
2020-03-03 11:30:04 +0100

    avancement

--- json/upload_files.php
+++ json/upload_files.php
@@ -30,13 +30,15 @@ if (isset($_FILES['image_file']))
     if(in_array($ext, $valid_extensions))
     {
         $path = realpath('./uploads/'); // upload directory
+        echo "path :\n"; var_dump($path);
         mkdir($path, 0777, true);
-        $path = $path.strtolower($final_image);
+        $path = $path . strtolower($final_image);
+        echo "path :\n"; var_dump($path);
 
-        echo "file"; var_dump($file);
-        echo "obj"; var_dump($obj);
-        echo "tmp"; var_dump($tmp);
-        echo "path"; var_dump($path);
+        echo "file :\n"; var_dump($file);
+        echo "obj :\n"; var_dump($obj);
+        echo "tmp :\n"; var_dump($tmp);
+
 
 
         if(move_uploaded_file($tmp, $path))
