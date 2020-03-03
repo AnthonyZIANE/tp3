d35331fb79f897cd76f2bdab0c4153681d0520d2
z18011806 <anthony.ziane@etu.univ-amu.fr>
2020-03-03 13:25:35 +0100

    avancement

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ $(document).ready(function (e) {
                         // view uploaded file.
                         if (data.hasOwnProperty('final_filename')) {
                             // $("#preview").html(data.final_filename).fadeIn();
-                            $("#centre2").append(
+                            $("#centre2")
+                                .append(
                                 $('<img />')
                                     .attr({'src': data.final_filename})
                                     .css({
